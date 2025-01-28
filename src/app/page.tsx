c9d3d45cce46337870d623525fbe46f6384d081c
Purwa Wiadnyana <purwawiadnyana08@gmail.com>
2025-01-28 15:57:10 +0800

    commit service section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import { loadingTimeout } from '@/lib/loading-timeout'
 import Hero from '@/components/sections/hero'
 import DescriptionTag from '@/components/sections/description-tag'
 import HighlightProject from '@/components/sections/highlight-project'
+import ServiceSection from '@/components/sections/service-section'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   const [loading, setLoading] = useState(true)
@@ -31,10 +33,45 @@ export default function Home() {
           <Hero />
         </div>
 
-        <div className="my-5 lg:my-20 2xl:my-24 h-full w-full">
-          <DescriptionTag />
+        <div className="my-5 lg:my-20 2xl:my-24 h-full w-full container mx-auto">
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{
+              once: false,
+              amount: 0.5,
+              margin: '0px 0px -200px 0px',
+            }}
+            transition={{ duration: 0.8 }}
+          >
+            <DescriptionTag />
+          </motion.div>
 
-          <HighlightProject />
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{
+              once: false,
+              amount: 0.5,
+              margin: '0px 0px -200px 0px',
+            }}
+            transition={{ duration: 0.8, delay: 0.5 }}
+          >
+            <ServiceSection />
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{
+              once: false,
+              amount: 0.5,
+              margin: '0px 0px -200px 0px',
+            }}
+            transition={{ duration: 0.8, delay: 0.5 }}
+          >
+            <HighlightProject />
+          </motion.div>
         </div>
       </section>
     </MainLayout>
